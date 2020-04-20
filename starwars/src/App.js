97e3294123ec172ddf42dcf194a89a13824f3ca5
Jason Hinton <jason_hinton@att.net>
2020-04-19 20:42:57 -0400

    styling issues

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,31 +8,31 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [people, setPeople] = useState([])
-
+  
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
     axios
-        .get('https://swapi.py4e.com/api/people/1/')
+        .get('https://swapi.py4e.com/api/people/')
         .then(response => {
-        console.log(response);
-            setPeople(response.data.results);
+        console.log(response.data);
+          setPeople(response.data.results);
         })
         .catch(error => {
             console.log("Error!!", error)
         });
   }, []);
 
-
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-      {people.map((person, index)  => (
-          <PeopleCard person={person} key={index}/>
-      ))} 
-    </div>
-  );
-}
+  console.log('People!', people);
+ return(
+   <div className='App'>
+     <h1 className="Header">React Wars</h1>
+     {people.map((person, index) => {
+       return <PeopleCard person={person} key={index}/>
+     })}
+  </div>
+ )
+};
 
 export default App;
--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,26 +1,31 @@
 import React from 'react';
- import { Card, CardHeader, CardFooter, CardBody,
+import { Card, CardHeader, CardFooter, CardBody,
   CardText, Col, Row } from 'reactstrap';
+import styled from 'styled-components';
+
+
 
 const PeopleCard = ({person}) => {
- return (  
-   <div className="container"> 
-     <Row xs="3">    
-        <Col xs="8" md="4">
-          <Card>
-              <CardHeader tag="h3">Name: {person.name}</CardHeader>
-              <CardBody>
-                <CardText>Height: {person.height}</CardText>
-                <CardText>Mass: {person.mass}</CardText>
-                <CardText>Birth Year: {person.birth_year}
-                  </CardText>
-              </CardBody>
-              <CardFooter className="text-muted">Gender: {person.gender}</CardFooter>
-            </Card>
-        </Col>
-      </Row>
-    </div>
- )};
+  return (  
+    <div className="container"> 
+      <Row>    
+         <Col>
+           <Card>
+               <CardHeader tag="h3">Name: {person.name}</CardHeader>
+               <CardBody>
+                 <CardText>Height: {person.height}</CardText>
+                 <CardText>Mass: {person.mass}</CardText>
+                 <CardText>Birth Year: {person.birth_year}</CardText>
+               </CardBody>
+               <CardFooter className="text-muted">Gender: {person.gender}</CardFooter>
+             </Card>
+         </Col>
+       </Row>
+       
+     </div>
+
+
+  )}
 
 
 
